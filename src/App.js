dd3e520f375f2fd420ad0a0e670d4e63e5f2303d
antonicasasayasg <antoni.casasayas1@gmail.com>
2021-08-26 17:31:12 +0200

    initial look done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="container">
+      <img id="logo" src="/logo.png" alt="google-logo" />
+      <input id="search-input" type="text" placeholder="Search Google or type a URL" />
+    </div>
+  );
+}
+
+export default App;
